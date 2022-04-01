951d3e56f94f96f604205352eed9e695fc178181
SCCMHON <97045607+SCCMHON@users.noreply.github.com>
2022-04-01 13:51:25 -0700

    Add files via upload

---
+++ osd.ps1
@@ -0,0 +1,27 @@
+﻿Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
+
+Start-Sleep -Seconds 5
+
+#Make sure I have the latest OSD Content
+
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+
+Install-Module OSD -Force
+
+Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
+
+Import-Module OSD -Force
+
+#Start OSDCloud ZTI the RIGHT way
+
+Write-Host -ForegroundColor Green "Start OSDCloud"
+
+Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
+
+#Restart from WinPE
+
+Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
+
+Start-Sleep -Seconds 20
+
+wpeutil reboot
