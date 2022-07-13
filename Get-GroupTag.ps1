1131e9793cb71d04fb93a8d0d2e8936e7ca9f859
SCCMHON <97045607+SCCMHON@users.noreply.github.com>
2022-07-13 10:25:04 -0700

    Add files via upload

--- Get-GroupTag.ps1
+++ Get-GroupTag.ps1
@@ -1,7 +1,6 @@
-﻿$clientId = "b70671ce-5631-42dc-82f4-536ac6aa2843" #Provide the Client ID
-$clientSecret = "K_2FiG3dQy2A6A-_KFgIXg_whN298_qB3m" # Provide the ClientSecret
-$ourTenantId = "96ece526-9c7d-48b0-8daf-8b93c90a5d18" #Specify the TenatID
-
+﻿$clientId = "b70671ce-5631-42dc-82f4-536ac6aa2843"
+$clientSecret = "K_2FiG3dQy2A6A-_KFgIXg_whN298_qB3m"
+$ourTenantId = "96ece526-9c7d-48b0-8daf-8b93c90a5d18"
 $Resource = "deviceManagement/windowsAutopilotDeviceIdentities"
 $Resource = "deviceManagement/managedDevices"
 $graphApiVersion = "Beta"
@@ -10,8 +9,15 @@ $authority = "https://login.microsoftonline.com/$ourTenantId"
 Update-MSGraphEnvironment -AppId $clientId -Quiet
 Update-MSGraphEnvironment -AuthUrl $authority -Quiet
 Connect-MSGraph -ClientSecret $clientSecret | Out-Null
-
-#$SerialNumbers = Get-Content -Path "SerialNumber.txt" #Provide the list of device you want to check the GroupTag
 $Serial = (Get-WmiObject -class win32_bios).SerialNumber
 $GT = Get-AutopilotDevice -serial $Serial | Select-Object -ExpandProperty groupTag
 $GT
+Clear-Host
+Start-Sleep 2
+Write-Host  "*****************************************" -F Yellow
+Write-Host  "*                                       *" -F Yellow
+Write-Host  "*                                       *" -F Yellow 
+Write-Host  "* " -f Yellow -NoNewline; Write-Host " Current Group TAG is: $GT" -f Green  -NoNewline; Write-Host  "  * " -f Yellow
+Write-Host  "*                                       *" -F Yellow
+Write-Host  "*                                       *" -F Yellow
+Write-Host  "*****************************************" -F Yellow
