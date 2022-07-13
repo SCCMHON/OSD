f9166ea9dd6cab399435a56790793b7631e1333f
SCCMHON <97045607+SCCMHON@users.noreply.github.com>
2022-07-13 10:32:34 -0700

    Add files via upload

--- osd.ps1
+++ osd.ps1
@@ -1,4 +1,33 @@
-﻿Write-Host  -ForegroundColor Green "Starting Honeywell OSDCloud ZTI"
+﻿Write-Host -ForegroundColor Green "Determing Current Group TAG"
+$clientId = "b70671ce-5631-42dc-82f4-536ac6aa2843"
+$clientSecret = "K_2FiG3dQy2A6A-_KFgIXg_whN298_qB3m"
+$ourTenantId = "96ece526-9c7d-48b0-8daf-8b93c90a5d18"
+$Resource = "deviceManagement/windowsAutopilotDeviceIdentities"
+$Resource = "deviceManagement/managedDevices"
+$graphApiVersion = "Beta"
+$uri = "https://graph.microsoft.com/$graphApiVersion/$($resource)"
+$authority = "https://login.microsoftonline.com/$ourTenantId"
+Update-MSGraphEnvironment -AppId $clientId -Quiet
+Update-MSGraphEnvironment -AuthUrl $authority -Quiet
+Connect-MSGraph -ClientSecret $clientSecret | Out-Null
+$Serial = (Get-WmiObject -class win32_bios).SerialNumber
+$GT = Get-AutopilotDevice -serial $Serial | Select-Object -ExpandProperty groupTag
+$GT
+Clear-Host
+Start-Sleep 2
+Write-Host  "*****************************************" -F Yellow
+Write-Host  "*                                       *" -F Yellow
+Write-Host  "*                                       *" -F Yellow 
+Write-Host  "* " -f Yellow -NoNewline; Write-Host " Current Group TAG is: $GT" -f Green  -NoNewline; Write-Host  "  * " -f Yellow
+Write-Host  "*                                       *" -F Yellow
+Write-Host  "*                                       *" -F Yellow
+Write-Host  "*****************************************" -F Yellow
+
+Write-Output "Please press any key to continue" -F Yellow
+[Console]::ReadKey() | Out-Null
+
+
+Write-Host  -ForegroundColor Green "Starting Honeywell OSDCloud ZTI"
 Start-Sleep -Seconds 5
 
 #Change Display Resolution for Virtual Machine
