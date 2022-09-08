64084bd45a215046234f94a0401d70e5674f744f
SCCMHON <97045607+SCCMHON@users.noreply.github.com>
2022-09-07 22:29:36 -0700

    Add files via upload

---
+++ NetTest.ps1
@@ -0,0 +1,36 @@
+﻿
+  Write-Host "Checking Internet Connectivity..." -ForegroundColor Yellow
+  $TestResult = (Test-NetConnection -ComputerName login.microsoftonline.com -Port 443).TcpTestSucceeded
+  if ($TestResult -eq $true){
+    Write-Host "Connection to login.microsoftonline.com .............. Succeeded." -ForegroundColor Green 
+  }else{
+    Write-Host "Connection to login.microsoftonline.com ................. failed." -ForegroundColor Red 
+  }
+
+  $TestResult = (Test-NetConnection -ComputerName device.login.microsoftonline.com -Port 443).TcpTestSucceeded
+  if ($TestResult -eq $true){
+    Write-Host "Connection to device.login.microsoftonline.com ......  Succeeded." -ForegroundColor Green 
+  }else{
+    Write-Host "Connection to device.login.microsoftonline.com .......... failed." -ForegroundColor Red 
+  }
+
+$TestResult = (Test-NetConnection -ComputerName autologon.microsoftazuread-sso.com -Port 443).TcpTestSucceeded
+if ($TestResult -eq $true){
+    Write-Host "Connection to autologon.microsoftazuread-sso.com ......  Succeeded." -ForegroundColor Green 
+}else{
+    Write-Host "Connection to autologon.microsoftazuread-sso.comm .......... failed." -ForegroundColor Red 
+}
+
+  $TestResult = (Test-NetConnection -ComputerName enterpriseregistration.windows.net -Port 443).TcpTestSucceeded
+  if ($TestResult -eq $true){
+    Write-Host "Connection to enterpriseregistration.windows.net ..... Succeeded." -ForegroundColor Green 
+  }else{
+    Write-Host "Connection to enterpriseregistration.windows.net ........ failed." -ForegroundColor Red 
+  }
+
+    $TestResult = (Test-NetConnection -ComputerName time.windows.com -Port 443).TcpTestSucceeded
+  if ($TestResult -eq $true){
+    Write-Host "Connection to time.windows.com ..... Succeeded." -ForegroundColor Green 
+  }else{
+    Write-Host "Connection to time.windows.com ........ failed." -ForegroundColor Red 
+  }
